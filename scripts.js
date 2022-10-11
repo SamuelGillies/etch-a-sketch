41ab4cbaa7a00ede9de94c303c6fbec5dd19d840
Sam Gillies <sam@samgillies.com>
2022-10-11 13:57:54 +0100

    cell appended to container

---
+++ scripts.js
@@ -0,0 +1,5 @@
+let container = document.getElementById('container'); 
+container.style.border = '2px solid blue';
+let cell = document.createElement('div');
+container.append(cell);
+cell.style.border = '2px solid green';
