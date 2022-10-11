6f42c7cc1855bdde24238190ce4b54b2752c256c
Sam Gillies <sam@samgillies.com>
2022-10-11 14:19:21 +0100

    class added to cell

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 let container = document.getElementById('container'); 
 container.style.border = '2px solid blue';
 let cell = document.createElement('div');
+cell.classList.add('cell'); 
 container.append(cell);
 cell.style.border = '2px solid green';
