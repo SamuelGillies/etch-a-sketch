bbd1d16ee84c70b1286f88bcbf9224bcf1ba7af2
Sam Gillies <sam@samgillies.com>
2022-10-11 15:06:37 +0100

    for loop instigated to create multiple cells

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
+// generate grid inside of container div 
+
 let container = document.getElementById('container'); 
-container.style.border = '2px solid blue';
-let cell = document.createElement('div');
-cell.classList.add('cell'); 
-container.append(cell);
-cell.style.border = '2px solid green';
+
+for (i=0; i<16; i++) {
+    let cell = document.createElement('div'); 
+    cell.className = 'cell'; 
+    container.append(cell);
+}
+
+// define active/inactive classes
+
+
+// detect click state, add or remove active/inactive class
