7239639f9b915fa4528e7bf6b1a052bb799667f7
Sam Gillies <sam@samgillies.com>
2022-10-12 15:49:22 +0100

    forEach loop successfully implemented to apply different classes to each cell based on mouse activity

--- scripts.js
+++ scripts.js
@@ -5,27 +5,34 @@ let container = document.getElementById('container');   // assign div #container
 function gridCreate(dim) {
     for (i = 0; i < dim; i++) {                         // loop through number of cells determined by dim value
         let cell = document.createElement('div');       // create a div for each loop
-        cell.className = 'cell';                        // assign each div a class of .cell for each loop 
+        cell.className = 'cell noHover';                        // assign each div a class of .cell for each loop 
+        cell.setAttribute("id", `cell${i}`);
         container.append(cell);                         // append each new div to the div #container for each loop
     }
-}
+}; 
 
-let dim = 64;                                           // set number of cells to create
-let dimRoot = Math.sqrt(dim);                           // calculate square root of number of cells to derive x and y values of grid
-gridCreate(dim);                                        // run function to create grid
-
-
-
-// resize grid-template-columns/rows based on dim size
+let dim = 16;                                           // set number of cells to create
+let dimSquare = Math.pow(dim, 2);                           // calculate square root of number of cells to derive x and y values of grid
+gridCreate(dimSquare);                                        // run function to create grid
 
 
+// add/subtract classes as necessary
 
+let cells = document.querySelectorAll('.cell');             // obtain nodelist with all items with class .cell
 
-// define active/inactive classes
+// hover active 
+cells.forEach(cell => cell.addEventListener('mouseenter', function() {      // loop through nodelist using forEach function, adding and removing classes as necessary
+    cell.classList.add('hover');
+    cell.classList.remove('noHover');
+}));
 
+// hover inactive
+cells.forEach(cell => cell.addEventListener('mouseout', function() {
+    cell.classList.add('noHover');
+    cell.classList.remove('hover');
+}));
 
-
-
-// detect click state, add or remove active/inactive class
-
-container.addEventListener('mouseover', hover());
+// after click
+cells.forEach(cell => cell.addEventListener('click', function() {
+    cell.classList.add('clicked');
+}));
